2c25fcbdbabd62cbb650f2595e93819b73532e99
Luis Felipe Borges <otaldolu.isborges4522@outlook.com.br>
2024-07-26 12:27:14 -0400

    Exercicio struct

---
+++ structs/exercicios/struct_livros.c
@@ -0,0 +1,30 @@
+/*ExercÃ­cio 1: Cadastro de Livros
+
+Crie uma struct Livro com os campos titulo, autor e ano. 
+Escreva uma função cadastrarLivro que receba como parâmetro uma struct Livro e a armazene em um vetor de structs Livro. 
+A funçao deve também imprimir os dados do livro cadastrado.
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Livros
+{
+    char titulo[50];
+    char autor[50];
+    int ano;
+    
+}; typedef struct Livros livros; 
+
+
+livros cadastrar_livros(livros livros)
+{
+
+}
+
+
+
+int main()
+{
+    return 0;
+}
