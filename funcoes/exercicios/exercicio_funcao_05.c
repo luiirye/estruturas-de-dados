b1dbc9e9835efe306ed7fd098f4a6a9fb762576f
Luis Felipe <otaldolu.isborges4522@outlook.com.br>
2024-07-30 17:32:21 -0400

    ponteiros

--- funcoes/exercicios/exercicio_funcao_05.c
+++ funcoes/exercicios/exercicio_funcao_05.c
@@ -9,6 +9,8 @@ Crie um programa em C que leia dois números inteiros e uma função que receba 
 
 int funcao_maior(int a, int b)
 {
+    int maior;
+    
     if(a > b)
     {
         return a;
@@ -18,17 +20,19 @@ int funcao_maior(int a, int b)
     {
         return b;
     }
+
+    return maior;
 }
 
 int main()
 {
     int num1, num2;
 
-    int jett = funcao_maior(num1, num2);
-
     printf("Digite dois numeros inteiros.\n");
     scanf("%d", &num1);
     scanf("%d", &num2);
+
+    int jett = funcao_maior(num1,num2);
 
     printf("numeros: %d e %d\n", num1, num2);
     printf("%d eh maior\n", jett);
