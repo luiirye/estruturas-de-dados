9b7a12a1413d327bfc93623fb81b5083a29444ae
Luis Felipe Borges <otaldolu.isborges4522@outlook.com.br>
2024-09-08 23:41:20 -0400

    lista

--- atividades faculdade/aula04/listas/lista_01.cpp
+++ atividades faculdade/aula04/listas/lista_01.cpp
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // biblioteca para alocação dinâmica
 #include <ctype.h>
 
 /* Crianção de uma lista encadeada ordenada */
 
 /*protótipos das funções*/
 int lista_vazia(node*);
-int insere_lista(node* , int);
+void insere_lista(node* , int);
+int busca_lista(node*, int);
 int remove_lIsta(node*);
-int busca_lista(node*);
-int imprime_lista(node*);
-int finaliza_lista(node*);
+void imprime_lista(node*);
+void finaliza_lista(node*);
 char menu();
 
 struct lista
@@ -18,36 +18,56 @@ struct lista
     int info;
     struct lista *proximo;
 
-};typedef struct lista node;
-// struct lista agora pode ser chamada usando "node"
+};typedef struct lista node; // Nó
+// struct Node agora pode ser chamada usando "node"
 
-/*Função que aloca um node na memória*/
-node *aloca()
+node aloca()
 {
-    node* aux;
-    aux = (node*)malloc(sizeof(node));
-    return aux;
+    node *aux;
+    aux = ((node) malloc(sizeof node));
+    return *aux;
 }
+// Função para alocar dinâmicamente;
 
 int lista_vazia(node* lista)
 {
-    if(lista == NULL)
+    if (lista == NULL)
     {
-        printf("a lista esta vazia!!");
+        return 1;
+    }
+	    
+    else
+    {
+        return 0;
     }
 }
 
-int insere_lista(node* lista, int info)
+void insere_lista(node* lista, int valor)
 {
-    node* novo;
+    node *novo;
+    // novo nó;
     novo = aloca();
-    if(novo == NULL);
-    {
-        printf("erro na alocacao de memoria");
-    } 
+    // novo nó foi alocado; 
+    novo -> info = valor;
+    // valor do nó recebido;
+    novo -> prox = lista;
+    // novo nó aponta para proximo recebendo o topo da lista;
+    return novo;
 }
 
+int busca_lista(node* lista, int valor)
+{
+    node *busca;
+    for(busca = lista ; busca != NULL; busca -> prox)
+    {
+        
+    }
+}
 
+int remove_lIsta(node* lista)
+{
+    lista *atual;
+}
 
 int main()
 {
