6369eeebddb5a13c5bea35b999d909eb4a853716
luiirye <borg.esluii01645@gmail.com>
2025-03-17 17:30:28 -0400

    função cria lista

--- conteudos/Listas/ligadas/linked.cpp
+++ conteudos/Listas/ligadas/linked.cpp
@@ -1,54 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// estrutura node 
 typedef struct Lista {
     
-    int num;
+    int num; // campo de informação do node
     struct Lista *proximo;
 
 }node;
 
-node *aloca();
-void iniciaLista(node);
-node insereInicio(node*, int);
-node insereMeio(node*, int);
-node insereFim(node*, int);
-node removeNode(int);
-void imprimeLista(node);
-int menu();
+// Funções de listas ligadas
 
+// Função para alocar um novo node
+node *aloca(){
+    node *aux = (node*) malloc (sizeof(node));
+    return aux;
+}
+
+// Função para criar uma lista
+node *criaLista(node *CABECA){
+    
+    node *novo = aloca(); // aloca um novo node dinâmicamente
+   
+    if(!novo){
+        printf("Erro ao alocar espaço em memória\n");
+    }
+
+    else{
+        CABECA = novo;
+        CABECA -> proximo = NULL;
+    }
+}
+
+node *insereNode(node *CABECA, int n){
+    node *aux = aloca(); //alocado um ponteiro auxiliar;
+
+    if(!criaLista){
+        printf("A lista está vazia.\n");
+    }
+
+    else{
+        CABECA -> proximo = aux -> proximo;
+        aux -> proximo = CABECA;
+    }
+}
 int main(){
+
+    node *C;
+    criaLista(C);
     return 0;
 }
 
-node *aloca(){
-    
-    node *aux = (node*) malloc (sizeof(node));
-    return aux;
-    
-}
-
-void iniciaLista(node *CABECA){    
-    
-    CABECA -> proximo = NULL;
-
-}
-
-
-node insereInicio(node *CABECA, int v){
-    
-    node *novo = aloca();
-    novo -> num = v;
-    novo -> proximo = CABECA;
-    return novo;
-
-}
-
-node insereFim(node *CABECA, int v){
-
-    node *auxiliar = aloca();
-    
-    while(auxiliar -> proximo != NULL){
-        
-    }
-}
