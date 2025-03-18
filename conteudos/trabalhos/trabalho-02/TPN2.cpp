7f60abf4969658cf3b897866f2c7d3634bca4afe
luiirye <borg.esluii01645@gmail.com>
2025-03-18 11:33:32 -0400

    TPN2

---
+++ conteudos/trabalhos/trabalho-02/TPN2.cpp
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// estrutura do Banco
+
+struct Banco{
+    int numero_conta;
+    char nome_cliente[30];
+    float saldo;
+    struct Banco *proximo;
+};
+typedef struct Banco Banco;
+
+// Funções para funcionalidade da lista encadeada Banco
+Banco *aloca(); // OK
+int Banco_vazio();
+void cria_banco();
+Banco *insere_cliente();
+Banco *remove_cliente();
+void imprime_cliente();
+int memu();
+
+int main(){
+    return 0;
+}
+
+// Função que aloca um espaço na memória dinâmicamente
+Banco *aloca(){
+    Banco *aux = (Banco*) malloc(sizeof(Banco));
+    return aux;
+}
+
+int Banco_vazio(Banco *CABECA){
+    if(CABECA -> proximo == NULL){
+        return 1;
+        // verdade, lista está vazia.
+    }
+
+    else{
+        return 0;
+        // falso, a lista Banco não está vazia.
+    }
+}
+
+void cria_banco(Banco *CABECA){
+    CABECA -> proximo = NULL;
+}
+
+Banco *insere_cliente(){
+    
+}
+
+Banco *remove_cliente(){
+    
+}
+
+void imprime_cliente(){
+
+}
